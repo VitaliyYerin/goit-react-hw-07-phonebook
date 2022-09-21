69bdf009134100257eea76939d30a66343fccf93
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-09-21 11:07:53 +0300

    change app.jsx

---
+++ src/App.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: auto;
+  max-width: 500px;
+  padding: 20px;
+  border-radius: 20px;
+  background-color: rgba(255, 255, 255, 0.6);
+  box-shadow: 10px 10px 26px 0px rgba(0, 0, 0, 0.75);
+  text-align: center;
+  opacity: 0.9;
+`;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
+import { Container } from 'App.styled';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import { Toaster } from 'react-hot-toast';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+      <Toaster
+        toastOptions={{
+          error: {
+            style: {
+              fontWeight: 500,
+              padding: '16px',
+              color: '#fff',
+              background: 'red',
+            },
+          },
+          success: {
+            style: {
+              fontWeight: 500,
+              padding: '16px',
+              color: '#fff',
+              background: '#5446cc',
+            },
+          },
+        }}
+      />
+    </Container>
   );
 };
+
+export default App;
